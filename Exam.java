438ad408b3e7c1b8917e4be85d8d5dfe0ef58e2c
Sadman1 <sadman1@163.com>
2015-03-05 15:39:35 +0000

    add questions

--- Exam.java
+++ Exam.java
@@ -1,7 +1,15 @@
 
+/**
+ * 请设计一个函数，把一个十六进制的数转换为二进制数
+ * 如：7B -> 1111011
+ **/
 
 public class Exam {
-    public static void main(String argv[]){
+    
+    public String hexToBin(String hex) {
+        return null;
+    }
+    public static void main(String argv[]) {
         System.out.println("Welcome to Lenovo Exam @2015");
     }
 }
